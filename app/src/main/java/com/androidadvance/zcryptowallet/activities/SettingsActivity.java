4d56ee089e8ba6bbf2996023f5550432a45818a6
Andrei <danandreist@gmail.com>
2018-01-12 15:14:31 +0200

    added change pin in settings

--- app/src/main/java/com/androidadvance/zcryptowallet/activities/SettingsActivity.java
+++ app/src/main/java/com/androidadvance/zcryptowallet/activities/SettingsActivity.java
@@ -17,8 +17,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import com.androidadvance.zcryptowallet.R;
 import com.androidadvance.zcryptowallet.data.local.PreferencesHelper;
+import com.androidadvance.zcryptowallet.data.remote.TheAPI;
 import com.androidadvance.zcryptowallet.utils.DialogFactory;
 import com.androidadvance.zcryptowallet.utils.SecurityHolder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -85,6 +91,65 @@ public class SettingsActivity extends AppCompatActivity {
 
         return true;
       });
+
+
+      Preference buttonChegePin = findPreference(getString(R.string.change_pin));
+      buttonChegePin.setOnPreferenceClickListener(preference -> {
+
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+            getActivity());
+        LayoutInflater inflater = (LayoutInflater) getActivity()
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.dialog_change_pin, null);
+        alertDialogBuilder.setView(view);
+        alertDialogBuilder.setCancelable(false);
+        final AlertDialog dialog = alertDialogBuilder.create();
+        dialog.show();
+        Button btn_update_pin_change = view.findViewById(R.id.btn_update_pin_change);
+        Button btn_update_pin_close = view.findViewById(R.id.btn_update_pin_close);
+        EditText editText_update_pin_public_address = view.findViewById(R.id.editText_update_pin_public_address);
+        EditText editText_update_pin_public_address_key = view.findViewById(R.id.editText_update_pin_public_address_key);
+        EditText editText_update_pin_pin = view.findViewById(R.id.editText_update_pin_pin);
+
+        btn_update_pin_close.setOnClickListener(v -> dialog.dismiss());
+
+        btn_update_pin_change.setOnClickListener(view1 -> {
+          String pin = editText_update_pin_pin.getText().toString().trim();
+          if(pin.length() < 4){
+            DialogFactory.error_toast(getActivity(),"Pin should be at least 4 characters").show();
+            dialog.dismiss();
+            return;
+          }
+
+          JsonObject updatePinJson = new JsonObject();
+          updatePinJson.add("publicaddress", new JsonPrimitive(editText_update_pin_public_address.getText().toString().trim()));
+          updatePinJson.add("publickey", new JsonPrimitive(editText_update_pin_public_address_key.getText().toString().trim()));
+          updatePinJson.add("new_pin", new JsonPrimitive(pin));
+
+          TheAPI theAPI = TheAPI.Factory.getIstance(getActivity());
+          theAPI.updateUserPin(updatePinJson).enqueue(new Callback<JsonObject>() {
+            @Override public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+              if(response.code() > 299){
+                DialogFactory.error_toast(getActivity(),"Your public address and key were incorrect. Please double-check.").show();
+                return;
+              }
+              DialogFactory.success_toast(getActivity(),"PIN Successfully Updated.").show();
+              dialog.dismiss();
+              SecurityHolder.storePIN(getActivity(), pin);
+            }
+
+            @Override public void onFailure(Call<JsonObject> call, Throwable t) {
+              DialogFactory.error_toast(getActivity(),"Your public address and key were incorrect. Please double-check.").show();
+            }
+          });
+
+
+        });
+
+        return true;
+      });
+
+
     }
 
     public static void showPrivateKeysDialog(Context context, String textToCopyToClipboard) {
